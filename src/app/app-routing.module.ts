2345c34062d99b662a54914cfed923f7c80627a0
LobS7er0 <ritzang@gmail.com>
2022-03-05 12:02:57 +0700

    add home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,9 @@ import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
-  { path: "", redirectTo: "Home", pathMatch: 'full' },
-  { path: 'Home', component: HomeComponent },
-  { path: '404', component: PageNotFoundComponent }
+  { path: "", redirectTo: "home", pathMatch: 'full' },
+  { path: "home", component: HomeComponent },
+  { path: "404", component: PageNotFoundComponent }
 ];
 
 @NgModule({
