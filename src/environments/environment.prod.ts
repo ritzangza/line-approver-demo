62a972f6d14a92c500babd33ed9716d5725c826e
LobS7er0 <ritzang@gmail.com>
2022-03-08 23:56:02 +0700

    env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  endPoint: "http://localhost:8080/jw/api",
+  endPoint: "https://c42e-223-204-219-85.ngrok.io/jw/api",
   liffId: '1656926061-0APpnkPM'
 };
