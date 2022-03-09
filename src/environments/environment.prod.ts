c3dbb4d4e49e71d7015cd463ba8f6dc42da7273a
LobS7er0 <ritzang@gmail.com>
2022-03-09 10:29:03 +0700

    update env local

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  endPoint: "http://authorwise.co.th:8080/jw/api",
+  endPoint: "https://d1cc-223-204-219-85.ngrok.io/jw/api",
   lineUrl: "https://line-approver-demo.herokuapp.com",
   liffId: "1656926061-0APpnkPM"
 };
